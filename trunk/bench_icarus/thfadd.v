08080d302ef77b2990afe2462703892cde78bfd9
ua3mqj@gmail.com <ua3mqj@gmail.com@a91445ae-8799-85e0-272e-585fa5480b22>
2015-03-02 19:33:03 +0000

    full adder (already done and, or, xor, half adder)

--- trunk/bench_icarus/thfadd.v
+++ trunk/bench_icarus/thfadd.v
@@ -6,17 +6,16 @@ output wire z0, z1, c0, c1;
 
 wire w1, w2;
 
-th23 th_0(ci0, x0, y0, c0);
-th23 th_1(ci1, x1, y1, c1);
+th23 th_0(ci0, x0, y0, w1);
+th23 th_1(ci1, x1, y1, w2);
 
-//assign c0 = w1;
-//assign c1 = w2;
+assign c0 = w1;
+assign c1 = w2;
 
 //assign z0 = 0;
 //assign z1 = 0;
 
-
-//th34w2 th_2(w2, ci0, y0, x0, z0);
-//th34w2 th_3(w1, ci1, y1, x1, z1);
+th34w2 th_2(w2, ci0, y0, x0, z0);
+th34w2 th_3(w1, ci1, y1, x1, z1);
 
 endmodule
